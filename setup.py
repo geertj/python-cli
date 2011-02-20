9ca524803cbc84a9476811105fbf7dfc97036b15
geertj <geertj@localhost>
2011-02-20 17:27:27 +0100

    PLY is needed as a setup_requires.

--- setup.py
+++ setup.py
@@ -71,7 +71,8 @@ version_info = {
 setup(
     package_dir = { '': 'lib' },
     packages = [ 'cli', 'cli.command', 'cli.platform', 'cli.platform.posix' ],
-    install_requires = [ 'distribute', 'ply >= 3.3' ],
+    setup_requires = [ 'ply >= 3.3' ],
+    install_requires = [ 'ply >= 3.3' ],
     entry_points = { 'console_scripts': [ 'cli-test = cli.main:main' ] },
     cmdclass = { 'build': mybuild, 'bdist_egg': mybdist_egg, 'gentab': gentab },
     use_2to3 = True,
